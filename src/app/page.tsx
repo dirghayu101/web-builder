c6f9073238dc52111c8e9a36443c1b009037252b
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2025-09-30 08:36:00 -0400

    Setup dependencies.

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page () {
+  return (
+
+    <>
+    <Button variant="destructive">
+      Click me!
+    </Button>
+    </>
+  );
+}
