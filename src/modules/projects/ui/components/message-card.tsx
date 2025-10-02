4c88a2d37bc4012e009a737e3e1a262634a70755
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2025-10-02 09:39:13 -0400

    Message component of project module done. Forms setup and connected procedures to the client side.

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -2,6 +2,8 @@ import type { Fragment, MessageRole, MessageType } from "@/generated/prisma";
 import { format } from "date-fns";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { ChevronRightIcon, Code2Icon } from "lucide-react";
 
 type MessageCardProps = {
   content: string;
@@ -17,6 +19,40 @@ type AssistantMessageProps = Omit<MessageCardProps, "role">;
 
 type UserMessageProps = {
   content: string;
+};
+
+type FragmentCardProps = {
+  fragment: Fragment;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+};
+
+const FragmentCard = ({
+  fragment,
+  isActiveFragment,
+  onFragmentClick,
+}: FragmentCardProps) => {
+  return (
+    <button
+      className={cn(
+        "flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary transition-colors",
+        isActiveFragment &&
+          "bg-primary text-primary-foreground border-primary hover:bg-primary"
+      )}
+      onClick={() => onFragmentClick(fragment)}
+    >
+      <Code2Icon className="size-4 mt-0.5" />
+      <div className="flex flex-col flex-1">
+        <span className="text-sm font-medium line-clamp-1">
+          {fragment.title}
+        </span>
+        <span className="text-sm">Preview</span>
+      </div>
+      <div className="flex items-center justify-center mt-0.5">
+        <ChevronRightIcon className="size-4" />
+      </div>
+    </button>
+  );
 };
 
 const UserMessage = ({ content }: UserMessageProps) => {
@@ -45,7 +81,13 @@ const AssistantMessage = ({
       )}
     >
       <div className="flex items-center gap-2 pl-2 mb-2">
-        {/* TODO: Add logo */}
+        <Image
+          src="/logo.svg"
+          alt="WebBuilder"
+          width={18}
+          height={18}
+          className="shrink-0"
+        />
         <span className="text-sm font-medium">Web Builder</span>
         <span className="text-xs text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
           {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
@@ -53,6 +95,13 @@ const AssistantMessage = ({
       </div>
       <div className="pl-8.5 flex flex-col gap-y-4">
         <span>{content}</span>
+        {fragment && type === "RESULT" && (
+          <FragmentCard
+            fragment={fragment}
+            isActiveFragment={isActiveFragment}
+            onFragmentClick={onFragmentClick}
+          />
+        )}
       </div>
     </div>
   );
