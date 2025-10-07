eeccfd62f18d60ca27ec18dff5190f0f51b1bd32
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2025-10-07 19:08:54 -0400

    Deployment.

--- prisma/migrations/20251007125839_usage_schema/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "web_builder_schema"."Usage" (
-    "key" TEXT NOT NULL,
-    "points" INTEGER NOT NULL,
-    "expire" TIMESTAMP(3),
-
-    CONSTRAINT "Usage_pkey" PRIMARY KEY ("key")
-);
